7dcd13a1f28381f4c8185aa09789a9f8264f2404
hamzaerrhh <hmzaeer@gmail.com>
2026-01-14 07:57:50 +0100

    adding go routine

---
+++ internal/types.go
@@ -0,0 +1,35 @@
+package internal
+
+type Artist struct {
+	ID           int      `json:"id"`
+	Image        string   `json:"image"`
+	Name         string   `json:"name"`
+	Members      []string `json:"members"`
+	CreationDate int      `json:"creationDate"`
+	FirstAlbum   string   `json:"firstAlbum"`
+	Locations    string   `json:"locations"`
+	ConcertDates string   `json:"concertDates"`
+	Relations    string   `json:"relations"`
+}
+
+type ArtistDetail struct {
+	Artist
+	LocationsData    *Locations
+	ConcertDatesData *Dates
+	RelationsData    *Relations
+}
+
+type Locations struct {
+	ID        int      `json:"id"`
+	Locations []string `json:"locations"`
+}
+
+type Dates struct {
+	ID    int      `json:"id"`
+	Dates []string `json:"dates"`
+}
+
+type Relations struct {
+	ID             int                 `json:"id"`
+	DatesLocations map[string][]string `json:"datesLocations"`
+}
--- main.go
+++ main.go
@@ -1,168 +1,43 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"html/template"
+	"grouping_tracker/internal"
 	"log"
 	"net/http"
-	"strconv"
+
+	"grouping_tracker/internal/handlers"
+	"grouping_tracker/internal/services"
 )
-
-// --- Structs ---
-
-type Artist struct {
-	ID           int      `json:"id"`
-	Image        string   `json:"image"`
-	Name         string   `json:"name"`
-	Members      []string `json:"members"`
-	CreationDate int      `json:"creationDate"`
-	FirstAlbum   string   `json:"firstAlbum"`
-	Locations    string   `json:"locations"`
-	ConcertDates string   `json:"concertDates"`
-	Relations    string   `json:"relations"`
-}
-
-type ArtistDetail struct {
-	Artist
-	LocationsData    *Locations
-	ConcertDatesData *Dates
-	RelationsData    *Relations
-}
-
-type Locations struct {
-	ID        int      `json:"id"`
-	Locations []string `json:"locations"`
-}
-
-type Dates struct {
-	ID    int      `json:"id"`
-	Dates []string `json:"dates"`
-}
-
-type Relations struct {
-	ID             int                 `json:"id"`
-	DatesLocations map[string][]string `json:"datesLocations"`
-}
-
-// --- Global Variables ---
 
 var (
-	artists   []Artist
-	templates *template.Template
+	artists []internal.Artist
 )
 
-// --- Main Logic ---
-
 func main() {
-	var err error
-	templates, err = template.ParseGlob("templates/*.html")
+	renderer, err := handlers.NewTemplateRenderer("templates/*.html")
 	if err != nil {
 		log.Fatal("Error loading templates:", err)
 	}
 
-	if err := fetchArtists(); err != nil {
+	svc := services.NewGroupieService(nil)
+	artists, err = svc.FetchArtists()
+	if err != nil {
 		log.Fatal("Error fetching artists:", err)
 	}
 
 	fs := http.FileServer(http.Dir("static"))
-	http.Handle("/static/", http.StripPrefix("/static/", fs))
+	mux := http.NewServeMux()
+	mux.Handle("/static/", http.StripPrefix("/static/", fs))
 
-	http.HandleFunc("/", homeHandler)
-	http.HandleFunc("/artist/", artistHandler)
+	h := handlers.New(handlers.Config{
+		Renderer: renderer,
+		Service:  svc,
+		Artists:  artists,
+	})
+	mux.HandleFunc("/", h.Home)
+	mux.HandleFunc("/artist/", h.Artist)
 
 	fmt.Println("Server starting on http://localhost:8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
-}
-
-func fetchArtists() error {
-	resp, err := http.Get("https://groupietrackers.herokuapp.com/api/artists")
-	if err != nil {
-		return err
-	}
-	defer resp.Body.Close()
-	return json.NewDecoder(resp.Body).Decode(&artists)
-}
-
-// --- Handlers ---
-
-func homeHandler(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/" {
-		render404(w)
-		return
-	}
-	err := templates.ExecuteTemplate(w, "index.html", artists)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-	}
-}
-
-func artistHandler(w http.ResponseWriter, r *http.Request) {
-	idStr := r.URL.Path[len("/artist/"):]
-	if idStr == "" {
-		http.Redirect(w, r, "/", http.StatusFound)
-		return
-	}
-	id, err := strconv.Atoi(idStr)
-	if err != nil || id < 1 || id > len(artists) {
-		render404(w)
-		return
-	}
-	// Safe ID Lookup
-	var selectedArtist Artist
-	found := false
-	for _, a := range artists {
-		if a.ID == id {
-			selectedArtist = a
-			found = true
-			break
-		}
-	}
-
-	if !found {
-		render404(w)
-		return
-	}
-
-	detail := ArtistDetail{Artist: selectedArtist}
-
-	if err := fetchData(selectedArtist.Locations, &detail.LocationsData); err != nil {
-		log.Printf("Error fetching locations: %v", err)
-		// Continue anyway - partial data is better than nothing
-	}
-
-	if err := fetchData(selectedArtist.ConcertDates, &detail.ConcertDatesData); err != nil {
-		log.Printf("Error fetching dates: %v", err)
-	}
-
-	if err := fetchData(selectedArtist.Relations, &detail.RelationsData); err != nil {
-		log.Printf("Error fetching relations: %v", err)
-	}
-
-	if err := templates.ExecuteTemplate(w, "artist.html", detail); err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-	}
-}
-
-func render404(w http.ResponseWriter) {
-	w.WriteHeader(http.StatusNotFound)
-	err := templates.ExecuteTemplate(w, "404.html", nil)
-	if err != nil {
-		http.Error(w, "404 Not Found", http.StatusNotFound)
-	}
-}
-
-// Helper function to reduce repetitive HTTP code
-func fetchData(url string, target interface{}) error {
-	resp, err := http.Get(url)
-	if err != nil {
-		return err
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode != http.StatusOK {
-		return fmt.Errorf("bad status: %d", resp.StatusCode)
-	}
-
-	return json.NewDecoder(resp.Body).Decode(target)
+	log.Fatal(http.ListenAndServe(":8080", mux))
 }
